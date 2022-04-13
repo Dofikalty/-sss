417ad7fd2dc02daba7c59e775038ad011e4467bd
Stiven Sigal <70148244+sadasd526@users.noreply.github.com>
2022-04-13 20:43:11 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,1 +1,1 @@
-local players = {'StivenTheDev', 'Bartek213710', 'OOOOmGGGG21', 'TradeeR1', 'Teas_feq', 'Crees_Man'}
+return {'StivenTheDev', 'Bartek213710', 'OOOOmGGGG21', 'TradeeR1', 'Teas_feq', 'Crees_Man'}
