f204fc868026d8fcc03385f0bc8c15dbad721acd
Stiven Sigal <70148244+sadasd526@users.noreply.github.com>
2022-04-13 20:49:11 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,1 +1,1 @@
-return {'StivenTheDev', 'Bartek213710', 'OOOOmGGGG21', 'TradeeR1', 'Teas_feq', 'Crees_Man'}
+return {'StivenTheDev', 'Bartek213710', 'OOOOmGGGG21', 'TradeeR1', 'Teas_feq', 'Crees_Man', 'ROBI_FAN1', 'LATERCRASHER'}
